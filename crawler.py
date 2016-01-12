51c8ce1167d57b899d66d0b7f6df60a7c528233e
aaghamohammadi <al.aghamohammadi@gmail.com>
2016-01-12 11:26:07 +0330

    create crawler package

--- crawler.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-r = requests.get('http://google.com')
-soup = BeautifulSoup(r.text, 'lxml')
-
-print(soup.prettify())
---
+++ main.py
@@ -0,0 +1,7 @@
+# from bs4 import BeautifulSoup
+# import requests
+#
+# r = requests.get('http://google.com')
+# soup = BeautifulSoup(r.text, 'lxml')
+#
+# print(soup.prettify())
